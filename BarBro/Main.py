ae4e3639e925e8476d6bfdcaf3ec47210ee83913
Monke <vecktor887@gmail.com>
2023-03-24 20:37:57 +0700

    base template with header

--- BarBro/Main.py
+++ BarBro/Main.py
@@ -2,6 +2,7 @@ import datetime
 
 from flask import Flask, render_template, redirect, abort, request
 from data import db_session
+from data.users import User
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
@@ -11,16 +12,15 @@ login_manager.init_app(app)
 
 
 def main():
-    db_session.global_init("db/blogs.db")
+    db_session.global_init("db/cocktails.db")
     db_sess = db_session.create_session()
     app.run()
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    return render_template("index.html")
 
 if __name__ == '__main__':
     main()
