f36a27077e3c246ae00c7115901991ebe4f06874
Monke <vecktor887@gmail.com>
2023-04-20 07:12:50 +0700

    try to take with glitch

--- start.sh
+++ start.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
-
-# Exit early on errors
-set -eu
-
-# Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
-export PYTHONUNBUFFERED=true
-
-# Install Python 3 virtual env
-VIRTUALENV=./venv
-
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
-
-# Install pip into virtual environment
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
-# Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-# Run your glorious application
-$VIRTUALENV/bin/python3 server.py
+python3 -m pip install -U pip
+python3 -m pip install -r requirements.txt
+python3 server.py
